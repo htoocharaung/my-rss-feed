927a1bab6d04e04f2613cf51b1616c6be5e4b105
candrania <90996730+candrania@users.noreply.github.com>
2021-12-19 19:04:45 +0530

    Multi

--- rss.py
+++ rss.py
@@ -58,7 +58,7 @@ def create_feed_checker(feed_url):
         last_id_from_db = db.get_link(feed_url).link
 
         if last_id_from_db == "*":
-            message = f"**{first_entry.title}**\n{first_entry.magneturl}"
+            message = f"**{first_entry.title}**\n```{first_entry.link}```"
             try:
                 app.send_message(log_channel, message)
                 if app2 is not None:
@@ -82,11 +82,32 @@ def create_feed_checker(feed_url):
                 break
 
             # ↓ Edit this message as your needs.
-            message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.magneturl})"           
+            if "eztv.re" in entry.link:   
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.links[1]['href']})"
+            elif "yts.mx" in entry.link:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.links[1]['href'])}"
+            elif "torlock" in entry.link:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.links[1]['href'])}"
+            elif "watercache" in entry.link:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.link)}"
+            elif "rarbg" in entry.link:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.link)}"
+            elif "etorrent.click" in entry.link:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.link)}"
+            else:
+                message = f"**{entry.title}**\n[⚜ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ ⚜]({entry.link)}"
+            try:
                 app.send_message(log_channel, message)
                 if app2 is not None:
-                    mirr_msg = f"{mirr_cmd} {entry.link}"
-                    app2.send_message(mirr_chat, mirr_msg)
+                    if "eztv.re" in entry.link:                        
+                        mirr_msg = f"{mirr_cmd} {entry.links[1]['href']}"
+                        app2.send_message(mirr_chat, mirr_msg)
+                    elif "yts.mx" in entry.link:
+                        mirr_msg = f"{mirr_cmd} {entry.links[1]['href']}"
+                        app2.send_message(mirr_chat, mirr_msg)
+                    else:
+                        mirr_msg = f"{mirr_cmd} {entry.link}"
+                        app2.send_message(mirr_chat, mirr_msg)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} seconds")
                 sleep(e.x)
