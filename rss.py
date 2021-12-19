08cca53bebfbba183c36d4268506630ab3452eba
candrania <90996730+candrania@users.noreply.github.com>
2021-12-19 17:29:59 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -58,7 +58,7 @@ def create_feed_checker(feed_url):
         last_id_from_db = db.get_link(feed_url).link
 
         if last_id_from_db == "*":
-            message = f"**{first_entry.title}**\n```{first_entry.torznab:attr name="magneturl"}}```"
+            message = f"**{first_entry.title}**\n```{first_entry.torznab:attr name="magneturl"}```"
             try:
                 app.send_message(log_channel, message)
                 if app2 is not None:
